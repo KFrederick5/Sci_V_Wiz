992a822889f1631e3d523918a22db52d9d09d2df
Jafke104 <eddevera@uci.edu>
2019-01-30 10:58:19 -0800

    Added script for attacker taking damage

---
+++ SciGuy_vs_WizKid/Assets/Scripts/Attackers/AttackerColliderHealth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerColliderHealth : MonoBehaviour
+{
+    public float health;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        health = 100.0f;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Bullet")
+        {
+            health -= 10;
+            Debug.Log("Current health is" + health);
+        }
+    }
+}
