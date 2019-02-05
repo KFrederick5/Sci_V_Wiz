d9c42a7efd51747283ffcdd360bea328fbc34f21
Jafke104 <eddevera@uci.edu>
2019-02-04 20:24:11 -0800

    Started tower placement scene

---
+++ SciGuy_vs_WizKid/Assets/PlacementManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject selected;
+    public GameObject possibleTower;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetTower()
+    {
+        selected = possibleTower;
+    }
+}
