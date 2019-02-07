748b24cb130baf06437075307d847cc7c0918947
MYOOOTT <mnguy1231@gmail.com>
2019-02-06 21:16:58 -0800

    Created main menu scene & added pause functionality

--- SciGuy_vs_WizKid/Assets/Scripts/UI/UIManager.cs
+++ SciGuy_vs_WizKid/Assets/Scripts/UI/UIManager.cs
@@ -7,6 +7,67 @@ public class UIManager: MonoBehaviour
 {
 
     public string MAIN_GAME;
+    GameObject[] pauseObjects;
+
+    void Start()
+    {
+        Time.timeScale = 1;
+        pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause"); 
+        hidePause();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            if (Time.timeScale == 1)
+                pause();
+            else if (Time.timeScale == 0)
+                unpause();
+        }
+    }
+    private void showPause()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(true);
+        }
+    }
+
+    private void hidePause()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(false);
+        }
+    }
+
+    public void pause() //the external commands
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            showPause();
+        } else
+        {
+            Debug.Log("Game is already paused!");
+        }
+    }
+
+
+    public void unpause()
+    {
+        if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+            hidePause();
+        } else
+        {
+            Debug.Log("Game is already unpaused!");
+        }
+    }
+
+
 
     public void StartGame()
     {
