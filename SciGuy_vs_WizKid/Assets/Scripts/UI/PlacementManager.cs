689dabb85220fa9bbd9064ed1c0d434fd15c5f2b
Jafke104 <eddevera@uci.edu>
2019-02-11 18:49:37 -0800

    Got tower select buttons working

---
+++ SciGuy_vs_WizKid/Assets/Scripts/UI/PlacementManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class PlacementManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public TowerBtn clickedBtn{ get; private set; }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!EventSystem.current.IsPointerOverGameObject())
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                PlaceTower();
+            }
+        }
+    }
+
+    public void SetTower(TowerBtn tower)
+    {
+        this.clickedBtn = tower;
+    }
+
+    public void PlaceTower() 
+    {
+
+        Debug.Log("Current Tower " + this.clickedBtn);
+    
+    }
+}
