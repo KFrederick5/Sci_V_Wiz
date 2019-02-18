e0ec238f158695582bb29430edcd3f8b389e4cf7
Dane Carstens <dcarsten@uci.edu>
2019-02-17 20:39:57 -0800

    Made unit manager scripts

--- SciGuy_vs_WizKid/Assets/Scripts/GameManager/AttackUnitComponent.cs
+++ SciGuy_vs_WizKid/Assets/Scripts/GameManager/AttackUnitComponent.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class AttackUnitComponent : MonoBehaviour
 {
+    public int cost = 100;
+    public float speed = 0.5f;
+    public float health = 15.0f
+    public float damage = 5.0f;
+    public float attack_speed = 0.67f;
+
     // Start is called before the first frame update
     void Start()
     {
--- SciGuy_vs_WizKid/Assets/Scripts/GameManager/DefenseUnitComponent.cs
+++ SciGuy_vs_WizKid/Assets/Scripts/GameManager/DefenseUnitComponent.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class DefenseUnitComponent : MonoBehaviour
 {
+    public int cost = 100;
+    public float health = 20.0f;
+    public float damage = 5.0f;
+    public float attack_speed = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
