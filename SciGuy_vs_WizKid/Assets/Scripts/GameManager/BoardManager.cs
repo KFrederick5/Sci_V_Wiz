29fcf9e0df6ffa34d47854e3ebe199e7e5295189
MYOOOTT <mnguy1231@gmail.com>
2019-02-20 16:44:33 -0800

    Gamemanager creates lanes now, added prefabs

--- SciGuy_vs_WizKid/Assets/Scripts/GameManager/BoardManager.cs
+++ SciGuy_vs_WizKid/Assets/Scripts/GameManager/BoardManager.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class BoardManager : MonoBehaviour
 {
-    public int columns = 16;
-    public int rows = 5;
+    public int rows = 10;
 
-    public GameObject grass;
-    public GameObject street;
+    public GameObject lane;
     public Transform grid;
 
     private void Start()
@@ -18,25 +16,10 @@ public class BoardManager : MonoBehaviour
     void SetupLevel()
     {
         grid = new GameObject ("Grid").transform;
-        for (int x = 0; x < columns; x++)
+        for (int y = 0; y < rows; y++)
         {
-            for (int y = 0; y < rows; y++)
-            {
-                if (x == 7)
-                {
-                    GameObject instance = Instantiate(street, new Vector3(x, y, 0f), Quaternion.identity, grid) as GameObject;
-                }
-                else if (x==8)
-                {
-                    GameObject instance = Instantiate(street, new Vector3(x, y, 0f), Quaternion.Euler(0,180,0), grid) as GameObject;
-
-                }
-                else
-                {
-                    GameObject instance = Instantiate(grass, new Vector3(x, y, 0f), Quaternion.identity, grid) as GameObject;
-                }
-                //vector3 bc i guess creating vector2 converts to vector3 anyways
-            }
+            GameObject instance = Instantiate(lane, new Vector3(0, y, 0f), Quaternion.identity, grid) as GameObject;
+            
         }
     }
 }
